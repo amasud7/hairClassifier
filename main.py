b821ace1605b58e2f7766d0a859f5a92ab216dc9
Ayad Masud <ayad.r.masud@gmail.com>
2024-07-27 18:21:55 -0500

    adding flask, html, css layout

---
+++ main.py
@@ -0,0 +1,18 @@
+import model
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template('index.html')
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+# how to call in this file
+print(model.learn.predict('test3.jpg')[0])
